5f93916b0b6a202ea0f0b65ef9df2abd7efa9b09
Christian Taylor <christianbtaylor@gmail.com>
2023-10-05 13:39:14 -0500

    Denote optional wish fields

--- resources/views/wishes/_fields.blade.php
+++ resources/views/wishes/_fields.blade.php
@@ -5,12 +5,18 @@
         <x-input name="name" :value="$wish->name" required autofocus />
     </x-field>
     <x-field>
-        <x-label for="url" :value="__('URL')" />
+        <x-label for="url">
+            {{ __('URL') }}
+            <span class="text-gray-600">(optional)</span>
+        </x-label>
         <x-error for="url" />
         <x-input name="url" :value="$wish->url" placeholder="https://..." />
     </x-field>
     <x-field>
-        <x-label for="description" :value="__('Notes')" />
+        <x-label for="url">
+            {{ __('Notes') }}
+            <span class="text-gray-600">(optional)</span>
+        </x-label>
         <x-error for="description" />
         <x-input name="description" :value="$wish->description" placeholder="Size, color, quantity, etc." />
     </x-field>
