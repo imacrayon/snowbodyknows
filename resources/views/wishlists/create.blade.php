9cb2d662fca88d7d208d253e54eece230f8bd462
Christian Taylor <christianbtaylor@gmail.com>
2023-11-06 21:42:08 -0600

    Place create & edit wishlist forms in a card

--- resources/views/wishlists/create.blade.php
+++ resources/views/wishlists/create.blade.php
@@ -1,17 +1,21 @@
 <x-app-layout>
-    <div class="max-w-7xl mx-auto px-4 space-y-6 sm:px-6 lg:px-8">
-        <x-slot name="title">
+    <x-slot name="title">
+        {{ __('New wishlist') }}
+    </x-slot>
+    <x-slot name="header">
+        <x-back href="{{ route('wishlists.index') }}">{{ __('Wishlists') }}</x-back>
+        <h1 class="font-semibold text-xl text-gray-800 leading-tight inline-flex items-center">
             {{ __('New wishlist') }}
-        </x-slot>
-        <x-slot name="header">
-            <x-back href="{{ route('wishlists.index') }}">{{ __('Wishlists') }}</x-back>
-            <h1 class="font-semibold text-xl text-gray-800 leading-tight inline-flex items-center">
-                {{ __('New wishlist') }}
-            </h1>
-        </x-slot>
-        <x-form method="post" action="{{ route('wishlists.store', $wishlist) }}">
-            @include('wishlists._fields')
-            <x-button-primary class="mt-8">{{ __('Create wishlist') }}</x-button-primary>
-        </x-form>
+        </h1>
+    </x-slot>
+    <div class="max-w-7xl mx-auto px-4 space-y-6 sm:px-6 lg:px-8">
+        <div class="p-4 sm:p-8 bg-white shadow rounded-lg">
+            <div class="max-w-xl">
+                <x-form method="post" action="{{ route('wishlists.store', $wishlist) }}">
+                    @include('wishlists._fields')
+                    <x-button-primary class="mt-8">{{ __('Create wishlist') }}</x-button-primary>
+                </x-form>
+            </div>
+        </div>
     </div>
 </x-app-layout>
--- resources/views/wishlists/edit.blade.php
+++ resources/views/wishlists/edit.blade.php
@@ -5,9 +5,13 @@
         </h1>
     </x-slot>
     <div class="max-w-7xl mx-auto px-4 space-y-6 sm:px-6 lg:px-8">
-        <x-form method="patch" action="{{ route('wishlists.update', $wishlist) }}">
-            @include('wishlists._fields')
-            <x-button-primary class="mt-8">Save changes</x-button-primary>
-        </x-form>
+        <div class="p-4 sm:p-8 bg-white shadow rounded-lg">
+            <div class="max-w-xl">
+                <x-form method="patch" action="{{ route('wishlists.update', $wishlist) }}">
+                    @include('wishlists._fields')
+                    <x-button-primary class="mt-8">Save changes</x-button-primary>
+                </x-form>
+            </div>
+        </div>
     </div>
 </x-app-layout>
