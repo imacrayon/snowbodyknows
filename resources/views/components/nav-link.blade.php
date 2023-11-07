b38887f21842435e11dea2a7efe639004d77a943
Christian Taylor <christianbtaylor@gmail.com>
2023-11-06 21:36:44 -0600

    Update hover/active styles in main nav

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -2,8 +2,8 @@
 
 @php
 $classes = ($active ?? false)
-            ? 'inline-flex items-center rounded-3xl px-4 py-3 text-sm font-medium leading-5 text-gray-900 bg-sky-50 hover:bg-sky-100'
-            : 'inline-flex items-center rounded-3xl px-4 py-3 text-sm font-medium leading-5 text-gray-600 hover:bg-sky-100';
+            ? 'flex items-center px-4 py-3 text-sm font-medium leading-5 text-gray-900 bg-sky-50 hover:bg-sky-100'
+            : 'flex items-center px-4 py-3 text-sm font-medium leading-5 text-gray-600 hover:bg-sky-100';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,18 +1,18 @@
 <nav class="flex divide-x">
-    <div class="p-2">
+    <div class="py-1.5 p-2">
         <div class="relative w-16 h-full">
             <img src="/img/snowman.svg" width="64" height="auto" class="absolute left-0 bottom-0" alt="SnowbodyKnows">
         </div>
     </div>
     <ul class="flex">
-        <li class="p-1">
+        <li>
             <x-nav-link :href="route('app')" :active="request()->routeIs('wishlists.index')">
                 {{ __('Wishlists') }}
             </x-nav-link>
         </li>
     </ul>
-    <x-dropdown align="bottom" width="48" class="flex items-center p-1 pl-2">
-        <x-slot name="button" class="rounded-3xl py-1 px-2 flex items-center bg-transparent">
+    <x-dropdown align="bottom" width="48" class="flex items-center p-0.5 pl-2">
+        <x-slot name="button" class="rounded-3xl py-0.5 px-2 flex items-center bg-transparent">
             <img src="{{ Auth::user()->avatar_url }}" width="32" height="32" class="rounded-full" alt="">
             <span class="sr-only">Settings</span>
             <x-phosphor-caret-down aria-hidden="true" class="ml-1 text-gray-400" width="16" height="16" />
