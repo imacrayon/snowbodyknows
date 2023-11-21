ad8214a4056d4166e0a50b91808d7bd005977dd7
Christian Taylor <christianbtaylor@gmail.com>
2023-11-21 14:13:56 -0600

    Move "register" call to action to top of the page

--- resources/views/layouts/guest-wishes.blade.php
+++ resources/views/layouts/guest-wishes.blade.php
@@ -13,17 +13,20 @@
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 pb-32">
             <main>
+                <div class="relative max-w-4xl mx-auto pt-6 mb-2 px-4 sm:px-6 lg:px-8">
+                    <div class="ribbon text-sm text-center">
+                        <p class="bg-red-300 text-red-900 p-3 sm:px-6 ">
+                            <a class="underline" href="{{ route('register') }}">Create an account</a> to save this wishlist & unlock more features.
+                        </p>
+                    </div>
+                </div>
                 @if (isset($header))
                     <div class="relative max-w-7xl mx-auto pt-12 pb-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
                 @endif
                 {{ $slot }}
-                <div class="relative max-w-7xl mx-auto pt-12 pb-6 px-4 sm:px-6 lg:px-8">
-                    <p>Want to get all features? <x-button-primary href="{{ route('login') }}">Login</x-button-primary></p>
-                </div>
             </main>
         </div>
     </body>
 </html>
-
