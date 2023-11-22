c3b2903e55e1aa2a48e87e370c4d64f572371ed7
Christian Taylor <christianbtaylor@gmail.com>
2023-11-22 15:25:24 -0600

    Add a.co affiliate domain

--- config/affiliates.php
+++ config/affiliates.php
@@ -2,4 +2,5 @@
 
 return [
     'amazon.com' => ['tag' => env('AFFILIATE_AMAZON_KEY'), 'linkCode' => null, 'linkId' => null, 'camp' => null, 'creative' => null],
+    'a.co' => ['tag' => env('AFFILIATE_AMAZON_KEY'), 'linkCode' => null, 'linkId' => null, 'camp' => null, 'creative' => null],
 ];
