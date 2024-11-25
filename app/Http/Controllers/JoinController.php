34cf6e3a856247589731665440e6db58d8915109
Christian Taylor <christianbtaylor@gmail.com>
2024-11-25 14:11:35 -0600

    Redirect already logged in users

--- app/Http/Controllers/JoinController.php
+++ app/Http/Controllers/JoinController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Models\Group;
+use Illuminate\Http\Request;
 
 class JoinController extends Controller
 {
-    public function __invoke(Group $group)
+    public function __invoke(Request $request, Group $group)
     {
+        if ($request->user()) {
+            return to_route('groups.users.create', $group);
+        }
+
         return view('join', [
             'group' => $group,
             'users' => $group->users,
